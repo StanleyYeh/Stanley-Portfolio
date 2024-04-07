09a9f9fec7abd1cb0c741c7c49652258871acaf4
StanleyYeh <yes0319@gmail.com>
2024-04-07 14:54:54 +0800

    Header

---
+++ app/contact/page.js
@@ -0,0 +1,7 @@
+export default function Contact({ children }) {
+  return (
+    <>
+      Contact Page
+    </>
+  );
+}
---
+++ app/projects/page.js
@@ -0,0 +1,7 @@
+export default function Projects({ children }) {
+  return (
+    <>
+      Projects Page
+    </>
+  );
+}
